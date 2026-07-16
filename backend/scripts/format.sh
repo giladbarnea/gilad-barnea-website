c313ae3118176593e533cb20ba49a0efdf1ed367
Gilad Barnea <giladbrn@gmail.com>
2026-07-16 20:06:55 +0300

    remove everything besides .git dir

--- backend/scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-set -x
-
-ruff check app scripts --fix --unsafe-fixes --preview
-ruff format app scripts --preview
--- backend/scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy app
-ruff check app --unsafe-fixes --preview
-ruff format app --check --preview
